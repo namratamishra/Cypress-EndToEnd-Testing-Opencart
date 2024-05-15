f15a30fd6143704b08b052e94d0a24a78cf8304d
namratamishra <namrataa.08@gmail.com>
2024-05-15 12:47:16 +0100

    2 Reoprts

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  video:true,
   e2e: {
     baseUrl:'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
    setupNodeEvents(on,config){
